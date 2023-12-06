fc6be86a1cb58a77002572f690f2fab85069bc85
Bassam Hesham <bssam2012@gmail.com>
2023-12-06 20:55:39 +0000

    Updating Yummy

--- apps/yummy2/flutter_service_worker.js
+++ apps/yummy2/flutter_service_worker.js
@@ -299,9 +299,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "d0fc769303a85b010c595ffd6c39448d
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "aec86bf2741bafd6de7592451f40ac61",
-"/": "aec86bf2741bafd6de7592451f40ac61",
-"main.dart.js": "7586432c0466571796f236beba5bcfc6",
+"index.html": "ed473f5c820af94c9759676d68987f71",
+"/": "ed473f5c820af94c9759676d68987f71",
+"main.dart.js": "f9c8b86e9bad32ab7e27eb5234f4692d",
 "manifest.json": "353359673a7db22a6027d6c060f0235a",
 "version.json": "e8ac56f1f8e90a45809b366cc9697132"};
 // The application shell files that are downloaded before a service worker can
