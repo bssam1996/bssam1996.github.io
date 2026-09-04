53e31f9a66aeaba0468e08deb38f4e4c03784fc2
Bassam Hesham <bssam2012@gmail.com>
2026-09-04 21:38:38 +0100

    Updating Yummy

--- apps/yummy2/flutter_bootstrap.js
+++ apps/yummy2/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2091113482" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2851445362" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
