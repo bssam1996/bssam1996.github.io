5606db168fd255845963d8ae49c19691d8ccba6b
Bassam Hesham <bssam2012@gmail.com>
2026-09-06 20:28:47 +0100

    Updating BalanceMate

--- apps/balancemate/flutter_bootstrap.js
+++ apps/balancemate/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2791056133" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1494875760" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
